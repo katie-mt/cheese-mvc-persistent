0bd4a835fafd367b4c4cce9e15bb68b8f8734e8b
Katie O'Brien <katie.obrien.mt@gmail.com>
2017-09-16 12:43:41 -0700

    Created Add handler #1 with Get request

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -2,7 +2,9 @@ package org.launchcode.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("category")
@@ -13,4 +15,19 @@ public class CategoryController {
     //private field categoryDao of type CategoryDao.  This is the mechanism with which we interact with objects stored in the database
     private CategoryDao categoryDao;
 
-}
+    // Request path: /category
+    @RequestMapping(value = "")
+    public String index(Model model) {
+        //returns a collection of all category objects managed by categoryDao
+        categoryDao.findAll();
+        //object that can be looped over that will provide all the cheeses in the database
+        model.addAttribute("categories", categoryDao.findAll());
+        return "jobs/category";
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+        public String Category(Model model) {
+            model.addAttribute("category", categoryDao.findAll());
+            model.addAttribute(new Category());
+            model.addAttribute("title", "Add Category");
+            return "category/add";
+    }
