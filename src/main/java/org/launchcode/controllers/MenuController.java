4c9999416ed59f77ca87124188b4062f624e3fa8
Katie O'Brien <katie.obrien.mt@gmail.com>
2017-09-18 16:57:44 -0700

    Implemented index method for Menu and created file for the index.html menu template

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -5,6 +5,7 @@ import org.launchcode.models.data.CheeseDao;
 import org.launchcode.models.data.MenuDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,4 +17,15 @@ public class MenuController {
 
     @Autowired
     private CheeseDao cheeseDao;
+
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        //returns a collection of all category objects managed by categoryDao
+        menuDao.findAll();
+        //object that can be looped over that will provide all the menu items in the database
+        model.addAttribute("menu", menuDao.findAll());
+
+        return "menu/index";
+    }
 }
