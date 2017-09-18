f9d7d0ae387aed1cd57aedb9c6469e22467078d4
Katie O'Brien <katie.obrien.mt@gmail.com>
2017-09-18 15:30:24 -0700

    Created and implemented the Menu class

---
+++ src/main/java/org/launchcode/models/Menu.java
@@ -0,0 +1,68 @@
+package org.launchcode.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Menu {
+
+    //id field should be a unique primary key
+    @Id
+    //Hibernate will generate the id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(min=3, max=15)
+    private String name;
+
+    //Sets up half of our many-to-many relationship
+    @ManyToMany
+    //@JoinColumn(name = "category_id")
+    //used to hold all items in the menu.  Hibernate will populate it for us based on the relationships
+    //set up in the controllers.
+    private List<Cheese> cheeses = new ArrayList<>();
+
+
+    //Hibernate will use this default constructor
+    public Menu() { }
+
+    //constructor that accepts a parameter to set name
+    public Menu(String name) {
+        this.name = name; }
+
+    //getter and setter for id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    //getter and setter for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //getter for cheeses (no setter!)
+    public List<Cheese> getCheeses() {
+        return cheeses;
+    }
+
+    //Method that adds the given object to the list
+    public void addItem(Cheese item)  {
+        this.item = item;
+    } {
+
+    }
+}
+
