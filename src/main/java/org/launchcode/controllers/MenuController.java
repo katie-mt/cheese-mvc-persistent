2ce7b56e19743c54b0fd441c9860a816b9f38294
Katie O'Brien <katie.obrien.mt@gmail.com>
2017-09-18 20:27:25 -0700

    Implemented view template for Menu

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -1,12 +1,19 @@
 package org.launchcode.controllers;
 
 
+import org.launchcode.models.Menu;
 import org.launchcode.models.data.CheeseDao;
 import org.launchcode.models.data.MenuDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(value = "menu")
@@ -37,5 +44,42 @@ public class MenuController {
         //passes a new menu item into the view
         model.addAttribute(new Menu());
         return "menu/add";
+    }
+
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model,
+                      @ModelAttribute @Valid Menu menu, Errors errors) {
+
+        //if we have errors, go back to add form and show error message
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add Menu");
+            return "menu/add";
+
+        }
+
+        //save new menu object
+        menuDao.save(menu);
+
+        //redirect to display the new menu item
+        return "redirect:view/" + menu.getId();
+    }
+
+    //display contents of a single menu
+    //path is view/the id of the menuId
+
+    @RequestMapping(value = "view/{menuId}", method = RequestMethod.GET)
+    public String viewMenu(Model model, @PathVariable int menuID) {
+
+        //fetch the particular menu that we're looking at
+        Menu menu = menuDao.findOne(menuID);
+        //title/name of menu
+        model.addAttribute("title", menu.getName())
+        //all cheeses part of the menu and pass into the view
+        model.addAttribute("cheeses", menu.getCheeses());
+        //grab specific id of the menu
+        model.addAttribute("menuId", menu.getId());
+
+        return "menu/view";
     }
 }
